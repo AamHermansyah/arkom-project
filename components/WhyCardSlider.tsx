432b45dcce3b864ae25c0e69a273fa5eb22db786
Aam Hermansyah <amzhermanzyah@gmail.com>
2023-11-27 10:03:30 +0700

    [UPDATE] Fix variabel

--- components/WhyCardSlider.tsx
+++ components/WhyCardSlider.tsx
@@ -6,75 +6,45 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa6";
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-const testimonials = [
+const spots = [
   {
     id: 1,
-    name: "John Doe",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    expert: "Ahli Gizi & Nutrisi",
     imageUrl: 'https://source.unsplash.com/300x600?doctor'
   },
   {
     id: 2,
-    name: "Jane Smith",
-    comment: "Praesent tincidunt consectetur turpis, eu aliquet nisi vestibulum sit amet. Nulla ac tellus tincidunt, lobortis lacus vitae, maximus mi.",
-    expert: "Ahli Gigi",
     imageUrl: 'https://source.unsplash.com/300x600?man'
   },
   {
     id: 3,
-    name: "Mike Johnson",
-    comment: "Nulla ac tellus tincidunt, lobortis lacus vitae, maximus mi. Etiam ullamcorper arcu vel dolor posuere, a ornare nibh luctus.",
-    expert: "Ahli Bedah",
     imageUrl: 'https://source.unsplash.com/300x600?people'
   },
   {
     id: 4,
-    name: "Sarah Davis",
-    comment: "Nulla ac tellus tincidunt, lobortis lacus vitae, maximus mi. Etiam ullamcorper arcu vel dolor posuere, a ornare nibh luctus.",
-    expert: "Ahli Kulit",
     imageUrl: 'https://source.unsplash.com/300x600?expert'
   },
   {
     id: 5,
-    name: "David Lee",
-    comment: "Nunc nec erat eu nunc interdum faucibus sed et ipsum. Etiam ullamcorper arcu vel dolor posuere, a ornare nibh luctus.",
-    expert: "Ahli Mata",
     imageUrl: 'https://source.unsplash.com/300x600?woman'
   },
   {
     id: 6,
-    name: "Emily Brown",
-    comment: "Nunc nec erat eu nunc interdum faucibus sed et ipsum. Etiam ullamcorper arcu vel dolor posuere, a ornare nibh luctus.",
-    expert: "Ahli Jantung",
     imageUrl: 'https://source.unsplash.com/300x600?law'
   },
   {
     id: 7,
-    name: "Daniel Wilson",
-    comment: "Nulla ac tellus tincidunt, lobortis lacus vitae, maximus mi.",
-    expert: "Ahli Psikologi",
     imageUrl: 'https://source.unsplash.com/300x600?student'
   },
   {
     id: 8,
-    name: "Olivia Taylor",
-    comment: "Curabitur lacinia nulla non efficitur malesuada.",
-    expert: "Ahli Fisioterapi",
     imageUrl: 'https://source.unsplash.com/300x600?agent'
   },
   {
     id: 9,
-    name: "Michael Anderson",
-    comment: "Sed nec sem gravida, aliquam enim ac, lobortis urna.",
-    expert: "Ahli Ginekologi",
     imageUrl: 'https://source.unsplash.com/300x600?hero'
   },
   {
     id: 10,
-    name: "Sophia Thomas",
-    comment: "Vivamus lacinia elit in eleifend tincidunt.",
-    expert: "Ahli Anestesiologi",
     imageUrl: 'https://source.unsplash.com/300x600?girl'
   },
 ];
@@ -126,7 +96,7 @@ const WhyCardSlider = () => {
         pagination={{ clickable: true }}
         ref={swiperRef}
       >
-        {testimonials.map((item) => (
+        {spots.map((item) => (
           <SwiperSlide key={item.id} className="relative lg:px-10 pb-20">
             <div key={item.id} className="overflow-hidden bg-gray-200 rounded-xl border-2">
               <div className="relative bg-gray-200 aspect-video">
